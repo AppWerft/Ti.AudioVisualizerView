1f2877e18081cb538379a8e985f43d9160867609
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-04-16 14:16:57 +0200

           

---
+++ android/build/generated/jni/ti.audiovisualizerview.VisualizerViewProxy.cpp
@@ -0,0 +1,419 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "ti.audiovisualizerview.VisualizerViewProxy.h"
+
+#include "AndroidUtil.h"
+#include "EventEmitter.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "Proxy.h"
+#include "ProxyFactory.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+#include "org.appcelerator.titanium.proxy.TiViewProxy.h"
+
+#define TAG "VisualizerViewProxy"
+
+using namespace v8;
+
+		namespace ti {
+		namespace audiovisualizerview {
+			namespace audiovisualizerview {
+
+
+Persistent<FunctionTemplate> VisualizerViewProxy::proxyTemplate = Persistent<FunctionTemplate>();
+jclass VisualizerViewProxy::javaClass = NULL;
+
+VisualizerViewProxy::VisualizerViewProxy(jobject javaObject) : titanium::Proxy(javaObject)
+{
+}
+
+void VisualizerViewProxy::bindProxy(Handle<Object> exports)
+{
+	if (proxyTemplate.IsEmpty()) {
+		getProxyTemplate();
+	}
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("VisualizerView");
+
+	Local<Function> proxyConstructor = proxyTemplate->GetFunction();
+	exports->Set(nameSymbol, proxyConstructor);
+}
+
+void VisualizerViewProxy::dispose()
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Dispose();
+		proxyTemplate = Persistent<FunctionTemplate>();
+	}
+
+	titanium::TiViewProxy::dispose();
+}
+
+Handle<FunctionTemplate> VisualizerViewProxy::getProxyTemplate()
+{
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate;
+	}
+
+	LOGD(TAG, "GetProxyTemplate");
+
+	javaClass = titanium::JNIUtil::findClass("ti/audiovisualizerview/VisualizerViewProxy");
+	HandleScope scope;
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("VisualizerView");
+
+	Handle<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(
+		titanium::TiViewProxy::getProxyTemplate()
+, javaClass, nameSymbol);
+
+	proxyTemplate = Persistent<FunctionTemplate>::New(t);
+	proxyTemplate->Set(titanium::Proxy::inheritSymbol,
+		FunctionTemplate::New(titanium::Proxy::inherit<VisualizerViewProxy>)->GetFunction());
+
+	titanium::ProxyFactory::registerProxyPair(javaClass, *proxyTemplate);
+
+	// Method bindings --------------------------------------------------------
+	DEFINE_PROTOTYPE_METHOD(proxyTemplate, "printMessage", VisualizerViewProxy::printMessage);
+	DEFINE_PROTOTYPE_METHOD(proxyTemplate, "getMessage", VisualizerViewProxy::getMessage);
+	DEFINE_PROTOTYPE_METHOD(proxyTemplate, "setMessage", VisualizerViewProxy::setMessage);
+
+	Local<ObjectTemplate> prototypeTemplate = proxyTemplate->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = proxyTemplate->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+	instanceTemplate->SetAccessor(String::NewSymbol("message"),
+			VisualizerViewProxy::getter_message
+			, VisualizerViewProxy::setter_message
+, Handle<Value>(), DEFAULT);
+
+	// Accessors --------------------------------------------------------------
+
+	return proxyTemplate;
+}
+
+// Methods --------------------------------------------------------------------
+Handle<Value> VisualizerViewProxy::printMessage(const Arguments& args)
+{
+	LOGD(TAG, "printMessage()");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		return titanium::JSException::GetJNIEnvironmentError();
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(VisualizerViewProxy::javaClass, "printMessage", "(Ljava/lang/String;)V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'printMessage' with signature '(Ljava/lang/String;)V'";
+			LOGE(TAG, error);
+				return titanium::JSException::Error(error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(args.Holder());
+
+	if (args.Length() < 1) {
+		char errorStringBuffer[100];
+		sprintf(errorStringBuffer, "printMessage: Invalid number of arguments. Expected 1 but got %d", args.Length());
+		return ThrowException(Exception::Error(String::New(errorStringBuffer)));
+	}
+
+	jvalue jArguments[1];
+
+
+
+
+	
+	
+	if (!args[0]->IsNull()) {
+		Local<Value> arg_0 = args[0];
+		jArguments[0].l =
+			titanium::TypeConverter::jsValueToJavaString(env, arg_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+
+	jobject javaProxy = proxy->getJavaObject();
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+				env->DeleteLocalRef(jArguments[0].l);
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+	}
+
+
+
+
+	return v8::Undefined();
+
+}
+Handle<Value> VisualizerViewProxy::getMessage(const Arguments& args)
+{
+	LOGD(TAG, "getMessage()");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		return titanium::JSException::GetJNIEnvironmentError();
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(VisualizerViewProxy::javaClass, "getMessage", "()Ljava/lang/String;");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'getMessage' with signature '()Ljava/lang/String;'";
+			LOGE(TAG, error);
+				return titanium::JSException::Error(error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(args.Holder());
+
+	jvalue* jArguments = 0;
+
+	jobject javaProxy = proxy->getJavaObject();
+	jstring jResult = (jstring)env->CallObjectMethodA(javaProxy, methodID, jArguments);
+
+
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+	if (env->ExceptionCheck()) {
+		Handle<Value> jsException = titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+		return jsException;
+	}
+
+	if (jResult == NULL) {
+		return Null();
+	}
+
+	Handle<Value> v8Result = titanium::TypeConverter::javaStringToJsString(env, jResult);
+
+	env->DeleteLocalRef(jResult);
+
+
+	return v8Result;
+
+}
+Handle<Value> VisualizerViewProxy::setMessage(const Arguments& args)
+{
+	LOGD(TAG, "setMessage()");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		return titanium::JSException::GetJNIEnvironmentError();
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(VisualizerViewProxy::javaClass, "setMessage", "(Ljava/lang/String;)V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'setMessage' with signature '(Ljava/lang/String;)V'";
+			LOGE(TAG, error);
+				return titanium::JSException::Error(error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(args.Holder());
+
+	if (args.Length() < 1) {
+		char errorStringBuffer[100];
+		sprintf(errorStringBuffer, "setMessage: Invalid number of arguments. Expected 1 but got %d", args.Length());
+		return ThrowException(Exception::Error(String::New(errorStringBuffer)));
+	}
+
+	jvalue jArguments[1];
+
+
+
+
+	
+	
+	if (!args[0]->IsNull()) {
+		Local<Value> arg_0 = args[0];
+		jArguments[0].l =
+			titanium::TypeConverter::jsValueToJavaString(env, arg_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+
+	jobject javaProxy = proxy->getJavaObject();
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+				env->DeleteLocalRef(jArguments[0].l);
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+	}
+
+
+
+
+	return v8::Undefined();
+
+}
+
+// Dynamic property accessors -------------------------------------------------
+
+Handle<Value> VisualizerViewProxy::getter_message(Local<String> property, const AccessorInfo& info)
+{
+	LOGD(TAG, "get message");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		return titanium::JSException::GetJNIEnvironmentError();
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(VisualizerViewProxy::javaClass, "getMessage", "()Ljava/lang/String;");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'getMessage' with signature '()Ljava/lang/String;'";
+			LOGE(TAG, error);
+				return titanium::JSException::Error(error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(info.Holder());
+
+	if (!proxy) {
+		return Undefined();
+	}
+
+	jvalue* jArguments = 0;
+
+	jobject javaProxy = proxy->getJavaObject();
+	jstring jResult = (jstring)env->CallObjectMethodA(javaProxy, methodID, jArguments);
+
+
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+	if (env->ExceptionCheck()) {
+		Handle<Value> jsException = titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+		return jsException;
+	}
+
+	if (jResult == NULL) {
+		return Null();
+	}
+
+	Handle<Value> v8Result = titanium::TypeConverter::javaStringToJsString(env, jResult);
+
+	env->DeleteLocalRef(jResult);
+
+
+	return v8Result;
+
+}
+
+void VisualizerViewProxy::setter_message(Local<String> property, Local<Value> value, const AccessorInfo& info)
+{
+	LOGD(TAG, "set message");
+	HandleScope scope;
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		LOGE(TAG, "Failed to get environment, message wasn't set");
+		return;
+	}
+
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(VisualizerViewProxy::javaClass, "setMessage", "(Ljava/lang/String;)V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'setMessage' with signature '(Ljava/lang/String;)V'";
+			LOGE(TAG, error);
+		}
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(info.Holder());
+	if (!proxy) {
+		return;
+	}
+
+	jvalue jArguments[1];
+
+	
+	
+	if (!value->IsNull()) {
+		Local<Value> arg_0 = value;
+		jArguments[0].l =
+			titanium::TypeConverter::jsValueToJavaString(env, arg_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+
+	jobject javaProxy = proxy->getJavaObject();
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+				env->DeleteLocalRef(jArguments[0].l);
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException();
+		env->ExceptionClear();
+	}
+
+
+
+
+}
+
+
+
+			} // namespace audiovisualizerview
+		} // audiovisualizerview
+		} // ti
