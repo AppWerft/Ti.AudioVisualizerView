8eac325bc05f02531048e63e9a140cee6108d4d5
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-04-20 12:01:36 +0200

          

--- example/app.js
+++ example/app.js
@@ -1,39 +1,15 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
 
+!function() {
+var Visualizer = require('ti.audiovisualizerview');
 
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
+var window = Ti.Ui.createWindow();
+window.add(Visualizer.createVisualizerView({
+    audioSessionId : Visualizer.AUDIOSESSION_OUTPUTMIX,  // for mixer output
+    renderType : Visualizer.TYPE_BAR
+}));
+Ti.Media.createAudioPlayer({
+    url:"http://audio.scdn.arkena.com/11016/fip-midfi128.mp3"
+}).start();
 
-// TODO: write your module tests here
-var audiovizualizerview = require('ti.audiovizualizerview');
-Ti.API.info("module is => " + audiovizualizerview);
-
-label.text = audiovizualizerview.example();
-
-Ti.API.info("module exampleProp is => " + audiovizualizerview.exampleProp);
-audiovizualizerview.exampleProp = "This is a test value";
-
-if (Ti.Platform.name == "android") {
-	var proxy = audiovizualizerview.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
-
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
-
+window.open();
+}();
